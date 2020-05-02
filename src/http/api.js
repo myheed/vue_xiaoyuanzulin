6adc1cfa7b08f15f7accc5f844e7dc23000b460f
myheed <2416999861@qq.com>
2020-05-02 08:49:22 +0800

    feat: 阿成服务器

--- src/http/api.js
+++ src/http/api.js
@@ -3,6 +3,8 @@ import store from '@/vuex/store.js'
 import router from '../router'
 
 const api = axios.create();
+// api.defaults.baseURL = '134.175.20.37';
+// api.defaults.timeout = 3030;
 api.defaults.baseURL = 'http://api.com';
 api.defaults.timeout = 5000;
 api.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
